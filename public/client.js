3655d50956b006871d2c4a95abec8f822076eaee
Loki899899 <lokeshsingh899@gmail.com>
2021-03-15 16:27:54 +0530

    v1.5

--- public/client.js
+++ public/client.js
@@ -10,7 +10,7 @@ iceServers = {
     iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
         {
-            urls: "numb.viagenie.ca",
+            urls: "turn:numb.viagenie.ca",
             username: "lokeshsingh899@gmail.com",
             credential: "Lokesh@numb899"
         },
